6702801c2b08f4969444b61a3928203485c16e88
BassamMutairi <mutairibassam@gmail.com>
2021-07-19 02:18:22 +0300

    feat: add unique id for each blog

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Header from './components/Header';
 import Body from './components/Body';
 import Blog from './components/Blog';
-
+import { uuid } from 'uuidv4'
 
 function App() {
 
@@ -12,7 +12,7 @@ function App() {
 
   const addBlogsHandler = (blog) => {
     // to save all the previous inputs and show them as a list
-    setBlog([ ...blogs, blog ])
+    setBlog([ ...blogs, {id: uuid(), ...blog}])
   }
 
     // get the data from the local storage
